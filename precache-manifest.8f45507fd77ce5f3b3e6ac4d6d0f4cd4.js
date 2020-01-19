f31d672bdb0dea892d301cc66cdc0a9c11c10fc4
Liviu <matliviu89@yahoo.com>
2020-01-19 22:44:15 +0200

    Updates

---
+++ precache-manifest.8f45507fd77ce5f3b3e6ac4d6d0f4cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4e6e67e26dc7f881f1a8ecc75bccaf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "39f1fb84341c0df5acb1",
+    "url": "/monsters-rolodex/static/css/main.b6d561f5.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39f1fb84341c0df5acb1",
+    "url": "/monsters-rolodex/static/js/main.75c5d66c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
